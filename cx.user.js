172ebf959d9a3cf1b25328dc0f4d0e2b23398794
Justin Su <injustsu@gmail.com>
2023-05-09 03:35:33 -0400

    Enable multicity search

--- cx.user.js
+++ cx.user.js
@@ -394,7 +394,7 @@
                     <a href="javascript:void(0);" class="tabs tab_flights">Flights</a>
                 </div>
                 <a href="javascript:void(0);" class="search_selected">${lang.search_selected} &raquo;</a>
-                <!--<a href="javascript:void(0);" class="search_multicity">${lang.book_multi} &raquo;</a>-->
+                <a href="javascript:void(0);" class="search_multicity">${lang.book_multi} &raquo;</a>
                 <div class="saved_flights"></div>
                 <div class="saved_queries"></div>
             </div>
@@ -595,7 +595,6 @@
         .unelevated_faves .saved_query input {
             vertical-align:-2px;
             margin-right:5px;
-            //display:none;
         }
         .unelevated_faves .saved_query:nth-child(odd){
             background: #f1efe6;
@@ -1984,7 +1983,7 @@
             }
             saved_list += `<div class="saved_flight" data-date="${date}" data-route="${from + to}">
             <label>
-                <!--<input type="checkbox" data-route="${date}${from}${to}" data-date="${date}">-->
+                <input type="checkbox" data-route="${date}${from}${to}" data-date="${date}">
                 <span>
                     <span class="sf_date">${toDashedDate(date)}</span>
                     <span class="sf_route">${from}-${stop ? stop + '-' : ''}${to}
@@ -2000,7 +1999,7 @@
 
                 </span>
             </label>
-            <!--<a href="javascript:void(0);" class="saved_book" data-book="true" "data-date="${date}" data-from="${from}" data-dest="${to}">${lang.query} &raquo;</a>-->
+            <a href="javascript:void(0);" class="saved_book" data-book="true" "data-date="${date}" data-from="${from}" data-dest="${to}">${lang.query} &raquo;</a>
             <span class="leg"></span>
             <a href="javascript:void(0);" class="saved_remove" data-remove="${fullquery}">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="saved_delete" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path> </svg>
