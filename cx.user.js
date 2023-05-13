3df42fe400c1de317e367d79ced65debe974009f
Justin Su <injustsu@gmail.com>
2023-05-13 10:29:46 -0400

    Remove redundant `shadowRoot.querySelector()`s

--- cx.user.js
+++ cx.user.js
@@ -1556,7 +1556,7 @@
 
         shadowRoot.querySelector('.unelevated_saved a').addEventListener('click', function(e) {
             // alert(JSON.stringify(saved))
-            shadowRoot.querySelector('.unelevated_faves').classList.toggle('unelevated_faves_hidden')
+            div_saved.classList.toggle('unelevated_faves_hidden')
         })
     };
 
@@ -1933,7 +1933,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="saved_delete" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path> </svg>
             </a></div>`
         })
-        shadowRoot.querySelector('.unelevated_faves .saved_queries').innerHTML = saved_list
+        div_saved_queries.innerHTML = saved_list
         shadowRoot.querySelector('.unelevated_saved a span').innerText = saved_arr.length
     }
 
@@ -2005,7 +2005,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="saved_delete" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"></path> </svg>
             </a></div>`
         })
-        shadowRoot.querySelector('.unelevated_faves .saved_flights').innerHTML = saved_list
+        div_saved_flights.innerHTML = saved_list
         shadowRoot.querySelector('.unelevated_saved a span').innerText = saved_arr.length
     }
 
