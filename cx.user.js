4f4cf9cdb2bd0261e90cc6274aa995b039ee7f3b
Justin Su <injustsu@gmail.com>
2023-05-14 07:35:18 -0400

    Remove unused `noFlights`

--- cx.user.js
+++ cx.user.js
@@ -2405,7 +2405,6 @@
 
         const heartSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="heart_save" viewBox="0 0 16 16"> <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path></svg>'
 
-        let noFlights = true
         let flightHTML = `<div data-from="${from}" data-to="${to}">
     <span class="flight_title">${from} - ${to}
     <a href="javascript:void(0)" class="bulk_save ${(saved[date + from + to] ? ' bulk_saved' : '')}" data-save="true" data-date="${date}" data-from="${from}" data-dest="${to}">${heartSvg}</a>
@@ -2470,7 +2469,6 @@
                         <span class="info_duration"><span>Total Flight Duration:</span> ${leg1Duration}</span>
                     </div>
                     `
-                        noFlights = false
                         flightHTML += '</div>'
                     }
                     if (savedFlights[flightKey]) {
@@ -2537,7 +2535,6 @@
                         <span class="info_duration"><span>Total Flight Duration:</span> ${leg1Duration}</span>
                     </div>
                     `
-                        noFlights = false
                         flightHTML += '</div>'
                     }
                     if (savedFlights[flightKey]) {
