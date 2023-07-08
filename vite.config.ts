9dbda9147a10a305c19ff1464e6f64e589d5ee4d
Justin Su <injustsu@gmail.com>
2023-07-08 17:26:39 -0400

    Vite: Set `build.minify` to `true`

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import monkey from 'vite-plugin-monkey'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        minify: true
+    },
     plugins: [
         monkey({
             entry: 'src/main.ts',
