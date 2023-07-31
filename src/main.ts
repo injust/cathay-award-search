f1d9800d7ac2112cea0c9ea76f24ef14fe32fef8
Justin Su <injustsu@gmail.com>
2023-07-31 00:00:58 -0400

    Use `lang.search_cont` for `btnBatch` too

--- src/main.ts
+++ src/main.ts
@@ -1203,6 +1203,7 @@ await (async () => {
 
     // cxString = newQueryPayload(uef_from, uef_to, uef_date, uef_adult, uef_child)
     log('cxString:', cxString)
+    btnBatch.innerHTML = `${loadingIconHtml} ${lang.searching_cont}`
     btnSearch.innerHTML = `${loadingIconHtml} ${lang.searching_cont}`
     btnSearch.classList.add('searching')
     const resp = await httpRequest('https://api.cathaypacific.com/redibe/standardAward/create', {
