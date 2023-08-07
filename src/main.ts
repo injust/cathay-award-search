6ddaec9836c99e12bfb4d15d1aaebe8346afa335
Justin Su <injustsu@gmail.com>
2023-08-07 16:01:25 -0400

    Bugfix: Change `bulkDate` from a booking link to plain text
    This didn't work properly when searching multiple airports

--- src/main.ts
+++ src/main.ts
@@ -444,9 +444,8 @@ await (async () => {
           stopBatch()
           el.innerText = lang.loading
           await regularSearch([{
-            // TODO: Why does this need a fallback value?
-            from: el.dataset.from ?? uef.from.substring(0, 3),
-            to: el.dataset.dest ?? uef.to.substring(0, 3),
+            from: el.dataset.from,
+            to: el.dataset.dest,
             date: el.dataset.date
           }], {
             adults: uef.adults,
@@ -543,9 +542,8 @@ await (async () => {
           stopBatch()
           el.innerText = lang.loading
           await regularSearch([{
-            // TODO: Why does this need a fallback value?
-            from: el.dataset.from ?? uef.from,
-            to: el.dataset.dest ?? uef.to,
+            from: el.dataset.from,
+            to: el.dataset.dest,
             date: el.dataset.date
           }], {
             adults: 1,
@@ -1373,8 +1371,8 @@ await (async () => {
       const resultsRow = `
         <tr data-date="${date}">
           <td class="bulkDate">
-            <a href="javascript:void 0" data-book data-date="${date}">${dateStringToDashedDateString(date)}</a>
-            ${dateToWeekday(dateStringToDate(date))}
+            <div>${dateStringToDashedDateString(date)}</div>
+            <div>${dateToWeekday(dateStringToDate(date))}</div>
           </td>
           <td class="bulk_flights"></td>
         </tr>
