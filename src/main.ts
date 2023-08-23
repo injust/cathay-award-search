5ba0c07ff117707b1619de119b2ac18668b3e81f
Justin Su <injustsu@gmail.com>
2023-08-22 22:46:58 -0400

    Add `scroll` event listener to `document` instead of `window`

--- src/main.ts
+++ src/main.ts
@@ -1493,7 +1493,7 @@ await (async () => {
     shadowContainer.appendChild(searchBox)
     assignElements()
     addFormListeners()
-    window.addEventListener('scroll', (e) => {
+    document.addEventListener('scroll', (e) => {
       stickyFooter()
     })
     updateSavedQueries()
