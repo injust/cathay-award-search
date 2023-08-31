a355aea924d810d504bb9a7a49ae48ec0467aab1
Justin Su <injustsu@gmail.com>
2023-08-31 02:05:20 -0400

    Convert `{f,j,p,y}` dataset properties to CSS classes

--- src/main.ts
+++ src/main.ts
@@ -1384,7 +1384,7 @@ await (async () => {
           if (available !== '') {
             flightHTML += `
             <div class="flight_wrapper">
-              <div class="flight_item direct ${savedFlights.has(flightKey) ? 'saved' : ''}" data-flight-key="${flightKey}" data-flight-avail="${numF}_${numJ}_${numP}_${numY}" ${numF > 0 ? 'data-f' : ''} ${numJ > 0 ? 'data-j' : ''} ${numP > 0 ? 'data-p' : ''} ${numY > 0 ? 'data-y' : ''}>
+              <div class="flight_item direct ${savedFlights.has(flightKey) ? 'saved' : ''} ${numF > 0 ? 'f' : ''} ${numJ > 0 ? 'j' : ''} ${numP > 0 ? 'p' : ''} ${numY > 0 ? 'y' : ''}" data-flight-key="${flightKey}" data-flight-avail="${numF}_${numJ}_${numP}_${numY}">
                 <img src="https://book.cathaypacific.com${staticFilesPath}common/skin/img/airlines/logo-${leg1Airline.toLowerCase()}.png" />
                 <span class="flight_num">${leg1Airline}${leg1FlightNum}</span>
                 ${available}
@@ -1436,7 +1436,7 @@ await (async () => {
           if (available !== '') {
             flightHTML += `
             <div class="flight_wrapper">
-              <div class="flight_item ${savedFlights.has(flightKey) ? 'saved' : ''}" data-flight-key="${flightKey}" data-flight-avail="${numF}_${numJ}_${numP}_${numY}" ${numF > 0 ? 'data-f' : ''} ${numJ > 0 ? 'data-j' : ''} ${numP > 0 ? 'data-p' : ''} ${numY > 0 ? 'data-y' : ''}>
+              <div class="flight_item ${savedFlights.has(flightKey) ? 'saved' : ''} ${numF > 0 ? 'f' : ''} ${numJ > 0 ? 'j' : ''} ${numP > 0 ? 'p' : ''} ${numY > 0 ? 'y' : ''}" data-flight-key="${flightKey}" data-flight-avail="${numF}_${numJ}_${numP}_${numY}">
                 <img src="https://book.cathaypacific.com${staticFilesPath}common/skin/img/airlines/logo-${leg1Airline.toLowerCase()}.png" />
                 <span class="flight_num">${leg1Airline}${leg1FlightNum}
                 <span class="stopover">${transitAirportCode}</span>
