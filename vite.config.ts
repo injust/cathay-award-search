39e031fb64cf72460ecb637bc060071f790d8dc8
Justin Su <injustsu@gmail.com>
2023-10-09 19:48:57 -0400

    TEMP: Disable `minify` build option because it breaks automatic `@grant`

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import monkey from 'vite-plugin-monkey'
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        minify: true
+        // minify: true // TODO: Breaks automatic @grant
     },
     plugins: [
         monkey({
